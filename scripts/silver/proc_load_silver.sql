b5cf996ba7602adc0dd885235e24b2b00d3ea52f
Lucas TIONOHOUE <lucastionohoue.pro@gmail.com>
2025-06-22 15:07:51 -0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -17,3 +17,57 @@ Usage Example:
     EXEC Silver.load_silver;
 ===============================================================================
 */
+
+CREATE OR ALTER PROCEDURE silver.load_silver AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '================================================';
+        PRINT 'Loading Silver Layer';
+        PRINT '================================================';
+		
+		SET @start_time = GETDATE();
+			PRINT '>> Truncating Table: silver.category_tree';
+			TRUNCATE TABLE silver.category_tree;
+			PRINT '>> Inserting Data Into: silver.category_tree';
+			INSERT INTO silver.category_tree (
+				category_id,
+				parent_id,
+				is_root
+			)
+			SELECT
+				category_id,
+				CAST(NULLIF(TRIM(CAST(raw_parent_id AS VARCHAR)), '') AS INT) AS parent_id,
+				CASE 
+					WHEN TRIM(raw_parent_id) IS NULL OR TRIM(CAST(raw_parent_id AS VARCHAR)) = '' THEN 1
+					ELSE 0
+				END AS is_root
+			FROM (
+				SELECT
+					CAST(TRIM(CAST(category_id AS VARCHAR)) AS INT) AS category_id,
+					TRIM(CAST(parent_id AS VARCHAR)) AS raw_parent_id,
+					ROW_NUMBER() OVER (
+						PARTITION BY CAST(TRIM(CAST(category_id AS VARCHAR)) AS INT)
+						ORDER BY parent_id
+					) AS rn
+				FROM bronze.category_tree
+			) AS cleaned
+			WHERE rn = 1;
+
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Silver Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+		
+	END TRY
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+END
